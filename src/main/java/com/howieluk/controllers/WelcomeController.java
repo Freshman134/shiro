0d2feadb59838194a250a1ec64b1870b5a376b83
かぐや <2424299117@qq.com>
2021-08-09 00:18:17 +0800

    添加日志输出

--- src/main/java/com/howieluk/controllers/WelcomeController.java
+++ src/main/java/com/howieluk/controllers/WelcomeController.java
@@ -2,6 +2,7 @@ package com.howieluk.controllers;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,26 +12,27 @@ import com.howieluk.pojo.User;
 import com.howieluk.services.EmptyService;
 
 @Controller
+@Slf4j
 public class WelcomeController {
 	@Autowired
 	EmptyService emptyService;
 	
-	@RequestMapping(value="/hello")
+	@RequestMapping("/hello")
 	public String welcome() {
-		System.out.println("welcome");
+		log.info("welcome");
 		return "index";
 	}
-	
-	@RequestMapping(value="/getAll")
-	@ResponseBody
-	public String getAll() {
-		List<User> resList = this.emptyService.getAll();
-		String res = "[";
-		for(User u: resList) {
-			res = res + u.toString() + ",";
-		}
-		res = res.substring(0, res.length() - 2) + "]";
-		return res;
-	}
+
+//	@RequestMapping(value="/getAll")
+//	@ResponseBody
+//	public String getAll() {
+//		List<User> resList = this.emptyService.getAll();
+//		String res = "[";
+//		for(User u: resList) {
+//			res = res + u.toString() + ",";
+//		}
+//		res = res.substring(0, res.length() - 2) + "]";
+//		return res;
+//	}
 	
 }
